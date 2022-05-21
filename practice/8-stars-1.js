a105a24a01b8bdeeb88d1ff958615e9487e2e881
anika noor <54759286+anikanoor111@users.noreply.github.com>
2022-05-21 17:58:11 +0000

    updtaed 8 and 9

--- practice/8-stars-1.js
+++ practice/8-stars-1.js
@@ -1,4 +1,4 @@
- m nb/*
+/*
 process.stdout.write(i + " ")
 write a function which prints stars like below
 
@@ -9,4 +9,14 @@ write a function which prints stars like below
 * * * * *
 
 */
+let n=6;
+function tableStar() {
+  for (let i=1; i<=n; i++){
+    for (let j=1; j<=i; j++){
+    process.stdout.write( "* ")
+    }
+   console.log()
+  }
 
+}
+tableStar()
--- practice/9-stars-2.js
+++ practice/9-stars-2.js
@@ -9,3 +9,16 @@ write a function which prints stars like below
 *
 
 */
+
+
+let n=6;
+function tableStar() {
+  for (let i=1; i<=n; i++){
+    for (let j=1; j<=n-i; j++){
+    process.stdout.write( "* ")
+    }
+   console.log()
+  }
+
+}
+tableStar()
